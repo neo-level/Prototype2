44ee802dccb7b31b8e677dc4ad91bf72bc6ab8bf
frederik desmet <info@neo-level.com>
2021-01-12 17:16:49 +0100

    Added variables as placeholder for speed and input, move char on key innput.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Add fields for to handle game object and player control.
+    public float horizontalInput;
+    public float speed = 10.0f;
 
     // Start is called before the first frame update
     private void Start()
@@ -15,8 +17,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        // Get the input type for horizontal movement.
+        horizontalInput = Input.GetAxis("Horizontal");
+        
         // Move player right or left key arrow pressed.
-
+        transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
+        
         // Make boundaries for the player area.
         
         // spawn random objects.
