71f3ed65120a38fc774e14640f7e2ca1732fb53e
frederik desmet <info@neo-level.com>
2021-01-12 17:48:50 +0100

    Code cleanup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,30 +7,35 @@ public class PlayerController : MonoBehaviour
     // Add fields for to handle game object and player control.
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10.0f;
 
     // Start is called before the first frame update
     private void Start()
     {
-        
     }
 
     // Update is called once per frame
     private void Update()
     {
-        // Make boundaries for the player area.
-        if (transform.position.x < -10)
+        // Check if player reaches the range treshold.
+        if (transform.position.x < -xRange)
         {
-            Vector3 leftPosition = transform.position;
-            transform.position = new Vector3(-10, leftPosition.y, leftPosition.z);
-
+            // Make a left boundary for the player area.
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+        else if (transform.position.x > xRange)
+        {
+            // Make a right boundary for the player area.
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
         // Get the input type for horizontal movement.
         horizontalInput = Input.GetAxis("Horizontal");
-        
+
         // Move player right or left key arrow pressed.
         transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
-        
-        
+
+
         // spawn random objects.
     }
 }
