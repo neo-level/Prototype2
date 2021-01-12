882923ef8ffaa8d1d8518bf8d5e11be53da18925
frederik desmet <print.fdesmet@gmail.com>
2021-01-12 16:51:09 +0100

    First comment lines.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Add fields for to handle game object and player control.
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Move player right or left key arrow pressed.
+
+        // Make boundaries for the player area.
     }
 }
