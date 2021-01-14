ffef6a5c0335f310c612ff6b86de92f5df31f5bf
frederik desmet <info@neo-level.com>
2021-01-14 09:46:17 +0100

    Destroy object when it leaves boundaries.

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBoundary = 30.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBoundary)
+        {
+            Destroy(gameObject);
+
+        }    
+    }
+}
