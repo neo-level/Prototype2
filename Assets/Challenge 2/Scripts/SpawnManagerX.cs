dc31aedb0471eb4ca38521f62ea88b3156259853
frederik desmet <info@neo-level.com>
2021-01-14 14:28:00 +0100

    made 2 random generators, one for the index another for the time.

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,22 +11,24 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        int spawnInterval = Random.Range(3, 6);
+        InvokeRepeating(methodName: nameof(SpawnRandomBall), time: startDelay, repeatRate: spawnInterval);
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
-        // Generate random ball index and random spawn position
+        // Generate random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
-    }
+        // Generate a random ball index.
+        int randomIndex = Random.Range(0, ballPrefabs.Length);
 
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[randomIndex], spawnPos, ballPrefabs[randomIndex].transform.rotation);
+    }
 }
