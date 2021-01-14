c48bd9ef98269b9cc45870a7f2049fac2055c770
frederik desmet <info@neo-level.com>
2021-01-14 11:40:48 +0100

    Addition of IvokeRepeating to spawn animals at a random given time interval.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,14 @@ public class SpawnManager : MonoBehaviour
 
     private float spawnRangeX = 20.0f;
     private float spawnPositionZ = 20.0f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     private void Start()
     {
+        // Spawn random animal on a time interval. Using an invoked method.
+        InvokeRepeating(methodName: nameof(SpawnRandomAnimal), time: startDelay, repeatRate: spawnInterval);
     }
 
     // Update is called once per frame
@@ -19,14 +23,20 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            // Random store an index from the array to the variable. 
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-
-            // Random generate the spawn position of the animals.
-            Vector3 spawnPosition = new Vector3(x: Random.Range(-spawnRangeX, spawnRangeX), y: 0, z: spawnPositionZ);
-
-            // Instantiate a prefab.
-            Instantiate(animalPrefabs[animalIndex], position: spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+            // Call method to generate an animal on a random location.
+            SpawnRandomAnimal();
         }
+    }
+
+    private void SpawnRandomAnimal()
+    {
+        // Random store an index from the array to the variable. 
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        // Random generate the spawn position of the animals.
+        Vector3 spawnPosition = new Vector3(x: Random.Range(-spawnRangeX, spawnRangeX), y: 0, z: spawnPositionZ);
+
+        // Instantiate a prefab.
+        Instantiate(animalPrefabs[animalIndex], position: spawnPosition, animalPrefabs[animalIndex].transform.rotation);
     }
 }
