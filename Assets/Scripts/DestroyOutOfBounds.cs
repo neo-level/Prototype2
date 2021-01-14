c7ce0af362c554dfdfe1490bfb52a7c05aed9197
frederik desmet <info@neo-level.com>
2021-01-14 09:53:35 +0100

    Destroy object when it leaves boundaries top and down.

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBoundary = 30.0f;
+    private float lowerBoundary = -10.0f;
     
     // Start is called before the first frame update
     void Start()
@@ -16,10 +17,18 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        // Check if the object crosses the boundary.
         if (transform.position.z > topBoundary)
         {
+            // Destroy object if the object passes the top boundary.
             Destroy(gameObject);
 
-        }    
+        } else if (transform.position.z < lowerBoundary)
+        {
+            // Destroy object if the object passes the down boundary.
+            Destroy(gameObject);
+
+        }
     }
 }
