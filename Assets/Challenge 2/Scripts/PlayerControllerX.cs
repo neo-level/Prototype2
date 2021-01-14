ee34cce4b56510ab2c119311747e7edf05682a63
frederik desmet <info@neo-level.com>
2021-01-14 14:43:56 +0100

    addition of timtracker preventing the spamming of dogs.

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-
+    
+    // Time placeholder.
+    public float spawnRate = 1.0f;
+    public float timestamp = 0.0f;
+    
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On space bar press, send dog. but prevent spam.
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= timestamp)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timestamp = Time.time + spawnRate;
         }
     }
 }
