bf56a7857bcdc9b953c1cbb5b3d824b8bf744565
frederik desmet <info@neo-level.com>
2021-01-14 12:57:29 +0100

    script for destroying on collision. when both objects interact destroy both.

---
+++ Assets/Scripts/DetectCollision.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Destroys the current game object.
+        Destroy(gameObject);
+        
+        // Destroys the object the current one interacts with. 
+        Destroy(other.gameObject);
+    }
+}
